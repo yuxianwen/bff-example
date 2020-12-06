fb14136b5485a4d1c6ff16046342d7dacbaee170
Xianwen Yu <xianwenyu@livetrade.com.cn>
2020-12-06 22:47:36 +0800

    配置压缩 copy 中的 css

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -1,10 +1,14 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const minify = require("html-minifier").minify;
 const path = require("path");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   output: {
     filename: "[name].[contenthash].js"
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin()]
   },
 
   plugins: [
